abe4fecff986231c0ee7cd0cc03644c862d24e5e
Yusuf Rizal <yusufrizalh@gmail.com>
2024-09-10 15:17:08 +0700

    aspnet-mvc

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+
+namespace MyProject.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show(int? id)
+        {
+            var category = new Category
+            {
+                CategoryId = id.HasValue ? id.Value : 0
+            };
+            return View(category);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Error()
+        {
+            return "I have an error here.";
+        }
+    }
+}
