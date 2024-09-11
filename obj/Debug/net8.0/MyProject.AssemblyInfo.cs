9c9b813271b7944419220dce67f7e8c49f5b7659
Yusuf Rizal <yusufrizalh@gmail.com>
2024-09-11 12:47:26 +0700

    tag helpers

--- obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2eec8d5d8c231c6c0dc82196ad72cf3511fcde0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489356576691a42b730580f3d8140882ffd4e12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
