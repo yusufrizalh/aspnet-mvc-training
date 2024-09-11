c2eec8d5d8c231c6c0dc82196ad72cf3511fcde0
Yusuf Rizal <yusufrizalh@gmail.com>
2024-09-11 10:15:46 +0700

    categories static repository

---
+++ Models/CategoriesRepository.cs
@@ -0,0 +1,65 @@
+﻿namespace MyProject.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category{CategoryId=1, CategoryName="Beverages", CategoryDescription="Beverages"},
+            new Category{CategoryId=2, CategoryName="Meals", CategoryDescription="Meals"},
+            new Category{CategoryId=3, CategoryName="Fruits", CategoryDescription="Fruits"},
+        };
+
+        // CREATE CATEGORY
+        public static void AddCategory(Category category)
+        {
+            var lastId = _categories.Max(x => x.CategoryId);
+            category.CategoryId = lastId + 1;
+            _categories.Add(category);
+        }
+
+        // READ ALL CATEGORIES
+        public static List<Category> GetAllCategories() => _categories;
+
+        // READ ONE CATEGORY BY ID
+        public static Category? GetCategoryById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(
+                x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                return new Category
+                {
+                    CategoryId = category.CategoryId,
+                    CategoryName = category.CategoryName,
+                    CategoryDescription = category.CategoryDescription,
+                };
+            }
+            return null;
+        }
+
+        // UPDATE CATEGORY
+        public static void UpdateCategory(
+            int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var CategoryToUpdate = GetCategoryById(categoryId);
+            if (CategoryToUpdate != null)
+            {
+                CategoryToUpdate.CategoryName = category.CategoryName;
+                CategoryToUpdate.CategoryDescription = category.CategoryDescription;
+            }
+        }
+
+        // DELETE CATEGORY
+        public static void DeleteCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(
+                x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe4fecff986231c0ee7cd0cc03644c862d24e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
